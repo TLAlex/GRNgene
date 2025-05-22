d85ad7a5511fe2bf210a989af3f0cb5a3f5a4130
Alexandre Tan-Lhernould <tlalex@hotmail.fr>
2025-05-22 17:20:42 +0200

    Draft of CMA ES optimization

--- grngene/GRNgene/__init__.py
+++ grngene/GRNgene/__init__.py
@@ -8,4 +8,4 @@ from .ODESystems.ODESystems import simulationODEs, getCoefficient  # noqa: F401
 
 from .Plot.plot import plotGraph, plotSim, plotProt, plot_grn_degree, plot_degree_network, plot_motifs_count, compare_motif_dist, compare_degree_distribution   # noqa: F401
 
-from .GRN.GRNCreationUtils import network_properties, adj_mx_gnw_goldstandard, LFRAlgorithm, adjacenteDiMatriceStaredFromGraph
+from .GRN.GRNCreationUtils import network_properties, adj_mx_gnw_goldstandard, LFRAlgorithm, adjacenteDiMatriceStaredFromGraph, BarabasiAlbertAlgorithm
